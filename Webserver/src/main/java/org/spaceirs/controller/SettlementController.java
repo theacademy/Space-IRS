6ba3087fc31ac0433b6527a24d28d17b12685ce9
a-nip <anip@wiley.com>
2024-04-11 17:15:28 +0100

    Controller changes to target services instead of repos

--- Webserver/src/main/java/org/spaceirs/controller/SettlementController.java
+++ Webserver/src/main/java/org/spaceirs/controller/SettlementController.java
@@ -2,6 +2,8 @@ package org.spaceirs.controller;
 
 import org.spaceirs.dao.SettlementRepo;
 import org.spaceirs.entity.Settlement;
+import org.spaceirs.services.ServicePersistenceException;
+import org.spaceirs.services.SettlementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,35 +16,35 @@ import java.util.List;
 @CrossOrigin
 public class SettlementController {
     @Autowired
-    private SettlementRepo settlementRepo;
+    private SettlementService settlementService;
 
     @GetMapping("/all")
     public ResponseEntity<List<Settlement>> allSettlements() {
-        List<Settlement> settlements = settlementRepo.findAll();
+        List<Settlement> settlements = settlementService.getAllSettlements();
         return ResponseEntity.status(HttpStatus.OK).body(settlements);
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<Settlement> getSettlement(@PathVariable("id") Integer id) {
-        Settlement settlement = settlementRepo.findById(id).orElse(null);
+    public ResponseEntity<Settlement> getSettlement(@PathVariable("id") Integer id) throws ServicePersistenceException {
+        Settlement settlement = settlementService.getSettlementById(id);
         return new ResponseEntity<Settlement>(settlement, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public ResponseEntity<Void> addSettlement(@RequestBody Settlement settlement) {
-        settlementRepo.save(settlement);
+    public ResponseEntity<Void> addSettlement(@RequestBody Settlement settlement) throws ServicePersistenceException {
+        settlementService.addNewSettlement(settlement);
         return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteSettlement(@PathVariable("id") Integer id) {
-        settlementRepo.deleteById(id);
+    public ResponseEntity<Void> deleteSettlement(@PathVariable("id") Integer id) throws ServicePersistenceException {
+        settlementService.deleteSettlementById(id);
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Settlement> updateSettlement(@PathVariable("id") Integer id, @RequestBody Settlement settlement) {
-        settlementRepo.save(settlement);
+    public ResponseEntity<Settlement> updateSettlement(@PathVariable("id") Integer id, @RequestBody Settlement settlement) throws ServicePersistenceException {
+        settlementService.updateSettlementData(id, settlement);
         return new ResponseEntity<Settlement>(settlement, HttpStatus.OK);
     }
 }
