78e08bc6121b2c372f5d2433a57918f7fc5ed92d
renvegh <rvegh@wiley.com>
2024-04-11 14:58:35 +0200

    settlementService skeleton added

--- Webserver/src/main/java/com/wiley/spaceIRS/service/SettlementService.java
+++ Webserver/src/main/java/com/wiley/spaceIRS/service/SettlementService.java
@@ -1,9 +1,13 @@
 package com.wiley.spaceIRS.service;
 
+import java.math.BigDecimal;
+
 public interface SettlementService {
     List<Settlement> getAllSettlements();
     Settlement getSettlementById(int id);
     Settlement addNewSettlement(Settlement settlement);
     Settlement updateSettlementData(int id, Settlement settlement);
     void deleteSettlementById(int id);
+    Settlement addSpeciesToSettlement(int setId, int speId, BigDecimal population);
+    Settlement addChildSettlementToSettlement(int setId);
 }
--- Webserver/src/main/java/com/wiley/spaceIRS/service/SettlementServiceImpl.java
+++ Webserver/src/main/java/com/wiley/spaceIRS/service/SettlementServiceImpl.java
@@ -1,7 +1,10 @@
 package com.wiley.spaceIRS.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class SettlementServiceImpl implements SettlementService {
@@ -13,6 +16,42 @@ public class SettlementServiceImpl implements SettlementService {
     }
 
     public List<Settlement> getAllSettlements(){
+        return settlementDao.getAllSettlements();
+    }
+
+    @Override
+    public Settlement getSettlementById(int id) {
+        Settlement settlement;
+        try{
+            settlement = settlementDao.findById(id);
+        } catch (DataAccessException ex){
+            // TODO create an exception with a correct message
+        }
+        return null;
+    }
+
+    @Override
+    public Settlement addNewSettlement(Settlement settlement) {
+        return null;
+    }
+
+    @Override
+    public Settlement updateSettlementData(int id, Settlement settlement) {
+        return null;
+    }
+
+    @Override
+    public void deleteSettlementById(int id) {
+
+    }
+
+    @Override
+    public Settlement addSpeciesToSettlement(int setId, int speId, BigDecimal population) {
+        return null;
+    }
+
+    @Override
+    public Settlement addChildSettlementToSettlement(int setId) {
         return null;
     }
 
